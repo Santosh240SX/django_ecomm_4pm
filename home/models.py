0cc2140c9f689ff6b35595dc6e28087bde14c484
Santosh <santosh@gmail.com>
2023-06-30 13:01:02 +0545

    m

--- home/models.py
+++ home/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    logo = models.CharField(max_length=200,blank = True)
+    slug = models.CharField(max_length=500, unique = True)
+
+    def __str__(self):
+        return self.name
+
+
+class Slider(models.Model):
+    name = models.CharField(max_length=500)
+    image = models.ImageField(upload_to='media')
+    description = models.TextField(blank= True)
+    rank = models.IntegerField()
+    url = models.URLField(bank = True, max_length=500)
+
+
+    def __str__(self):
+        return self.name
+
--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,5 @@ from django.urls import path,include
 from .views import *
 
 urlpatterns = [
-    path('',HomeView.as_view(), name='home'),
+     path('',HomeView.as_view(), name='home'),
 ]
